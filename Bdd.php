1fb1d59f6a2ef55134692f765a1b13534fae3a33
nicolasRagazzi2019 <nicolas.Ragazzi@gmail.com>
2019-10-30 15:43:35 +0100

    modification requete

--- Bdd.php
+++ Bdd.php
@@ -1,69 +1,68 @@
 <?php
 class Bdd {
-    private $_servername = 'localhost';
-    private $_username = 'root';
-	private $_password = '';
-    private $_conn;
+  private $_servername = 'localhost';
+  private $_username = 'root';
+  private $_password = '';
+  private $_conn;
 
-    public function connect() {
+  public function connect() {
 
-		$this->_conn = new mysqli($this->_servername, $this->_username, $this->_password); 
-		
-		mysqli_query($this->_conn, "SET character_set_results = 'utf8', character_set_client = 'utf8', 
-			character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
-             
-		//On vérifie la connexion 
-		if($this->_conn->connect_error){ 
-			die('Erreur : ' .$this->_conn->connect_error); 
-		} 
-		//echo 'Connexion réussie<br>';
-		
+    $this->_conn = new mysqli($this->_servername, $this->_username, $this->_password);
+
+    mysqli_query($this->_conn, "SET character_set_results = 'utf8', character_set_client = 'utf8',
+    character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+
+    //On vï¿½rifie la connexion
+    if($this->_conn->connect_error){
+      die('Erreur : ' .$this->_conn->connect_error);
+    }
+    //echo 'Connexion rï¿½ussie<br>';
+
+  }
+
+  function close() {
+
+    mysqli_close($this->_conn);
+  }
+
+  public function rechercheProfessionnel()
+  {
+    // sql
+    $sql = "SELECT designation,adresse, codePostal, ville, numTel  FROM annuaire.professionnel where designation like '%".$_POST['designation']."%'";
+
+    if ($result = mysqli_query($this->_conn, $sql)) {
+      //echo "Recherche ok<br>";
+    } else {
+      $this->printError();
+    }
+    return $result;
+  }
+
+  public function rechercheParticulier()
+  {
+
+    // sql
+    $sql = "SELECT nomPrenom, adresse, codePostal, ville, numTel
+        FROM annuaire.particulier
+        where (nomPrenom like '%".$_POST['nom']."%".$_POST['prenom']."%')
+        and codePostal like '%".$_POST['cp']."%'
+        and ville like '%".$_POST['ville']."%'";
+
+
+      if ($result = mysqli_query($this->_conn, $sql)) {
+        //echo "Recherche ok<br>";
+      } else {
+        $this->printError();
+      }
+
+      return $result;
     }
 
-	function close() {
+    public function printError(){
 
-		mysqli_close($this->_conn);
-	}
-
-	public function rechercheProfessionnel()
-	{
-		// sql
-		$sql = "SELECT designation,adresse, codePostal, ville, numTel  FROM annuaire.professionnel where designation like '%".$_POST['designation']."%'";
-
-		if ($result = mysqli_query($this->_conn, $sql)) {
-			//echo "Recherche ok<br>";
-		} else {
-			$this->printError();
-		}
-    return $result;
-	}
-
-	public function rechercheParticulier()
-	{
-
-		// sql 
-		$sql = "SELECT nomPrenom, adresse, codePostal, ville, numTel 
-		FROM annuaire.particulier 
-		where (nomPrenom like '%".$_POST['nom']."%'
-		or nomPrenom like '%".$_POST['prenom']."%')
-		and codePostal like '%".$_POST['cp']."%'
-		and ville like '%".$_POST['ville']."%'";
+      echo "Error SQL : " . mysqli_error($this->_conn) . "<br>";
+    }
+  }
 
 
-		if ($result = mysqli_query($this->_conn, $sql)) {
-			//echo "Recherche ok<br>";
-		} else {
-			$this->printError();
-		}
-
-		return $result;
-	}
-
-	public function printError(){
-
-		echo "Error SQL : " . mysqli_error($this->_conn) . "<br>";
-	}
-}
-
-
-?>
+  ?>
